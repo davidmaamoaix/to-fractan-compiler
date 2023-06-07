dc86aa98bdc9e6009ceea7bbd7bb15923f2c9ba2
BuildTools <davidmaamoaix@gmail.com>
2023-06-07 22:56:31 +0800

    refractor

--- fractran/procedures.py
+++ fractran/procedures.py
@@ -8,13 +8,19 @@ from .emit_context import EmitContext
 UnmappedCodeSegment = List[Tuple[List[int], List[int]]]
 
 
+class ParamType(enum):
+    IN = 0
+    OUT = 1
+
+
 class UnmappedCode:
     """
     Resembles a section of code containing unmapped instructions waiting to be
     assigned concrete registers during procedure relocation. It contains a set
-    of unmapped variables and the unmapped instructions.
+    of unmapped variables (local variables) and the unmapped instructions.
 
-    Unmapped values are represented by negative integers.
+    Unmapped values are represented by negative integers. Each negative number
+    is local to its `UnmappedCode`.
 
     Each tuple contains two lists: nominator and denominator. After mapping
     negative values to concrete primes, the product of each list becomes the
@@ -30,18 +36,37 @@ class UnmappedCode:
 
     def unmap_size(self) -> int:
         return len(self.remap)
+    
+
+class Procedure:
+
+    # on initialization
+    locals_count: int
+    params_type: List[ParamType]
+    code: List[UnmappedCode]
+
+    # after prime allocation
+    index: int
+    params_index: List[int]
+
+    def __init__(self, code: List[UnmappedCode]):
+        pass
+        
 
 
 # (macro) destructive move: a -> b
 def move(a: int, b: int) -> UnmappedCode:
-    return [(b, a)]
+    return UnmappedCode([([b], [a])])
 
 
 # (macro) destructive duplication: a -> b & c
 def duplicate(a: int, b: int, c: int) -> UnmappedCode:
-    return [(b * c, a)]
+    return UnmappedCode([([b, c], [a])])
 
 
 # (macro) move: a -> b
 def copy(a: int, b: int) -> UnmappedCode:
-    pass
+    return UnmappedCode([
+        ([b, 2], [a]),
+        ([a], [2])
+    ])
---
+++ fractran/program.py
@@ -0,0 +1,11 @@
+from typing import Dict
+
+from .procedures import Procedure
+
+
+class Program:
+
+    procs: Dict[str, Procedure]
+
+    def __init__(self):
+        pass
