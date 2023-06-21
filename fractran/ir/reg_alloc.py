c83f40e7919cad994e7683c69aad2fe4e8aa16f8
BuildTools <davidmaamoaix@gmail.com>
2023-06-21 21:19:56 +0800

    braindead reg alloc

--- fractran/ir/reg_alloc.py
+++ fractran/ir/reg_alloc.py
@@ -5,5 +5,6 @@ from typing import Dict
 # TODO: implement a Riggs' allocator (as TAC isn't a chordal graph)
 # braindead implementation of reg alloc cuz i wanna get this finished before
 # dmp end qwq
-def reg_alloc() -> Dict[str, int]:
-    pass
+def reg_alloc(g: nx.DiGraph) -> Dict[str, int]:
+    nodes = list(g.nodes())
+    return {n: id for id, n in enumerate(nodes)}
